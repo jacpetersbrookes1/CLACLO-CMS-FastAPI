0cb17edb969149d4fe8817a1a974352f51230f58
Jacqueline Peters <jacquipeters88@gmail.com>
2024-06-29 20:34:24 +0100

    created routers for api endpoints and added courses and sections to api endpoints

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from typing import Optional, List
 from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
+from api import users, courses, sections
+
+
 app = FastAPI(
     
     title="CLACLO CMS FastAPI",
@@ -14,35 +17,10 @@ app = FastAPI(
     license_info={
         "name": "MIT",
     },
-    
-    
-    
-    
+       
 )
 
+app.include_router(users.router)
+app.include_router(courses.router)
+app.include_router(sections.router)
 
-users = []
-
-class User(BaseModel):
-    email: str
-    is_active: bool
-    bio: Optional[str]
-
-@app.get("/users", response_model=List[User])
-async def get_users():
-    return users
-
-
-@app.post("/users")
-async def create_user(User):
-    users.append(user)
-    return "Success"
-
-
-@app.get("/users/{id}")
-async def get_user(
-        id: int = Path(...,
-                       description="The ID of the user you want to retrieve.", gt=2),
-        q: str = Query(None, max_length=5)
-):
-    return {"user": users[id], "query": q}
